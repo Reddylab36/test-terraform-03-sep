5ab939351e0752a19f4d34efe7d08f210b321fed
Reddylab36 <shekharreddy36@outlook.com>
2024-09-03 21:39:24 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "my-pets" {
-  filename = "pets.txt"
-  content = "The cat name is King"
+  filename = "dogs.txt"
+  content = "The dog name is King"
 }
 resource "random_pet" "petname" {
   prefix = "MR"
